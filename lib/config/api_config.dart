6fc9c7bb3cdec10165daf5a7a854d5418e37cef3
Suvijak248 <67030248@kmitl.ac.th>
2026-07-10 00:39:37 +0700

    ส่งงาน

---
+++ lib/config/api_config.dart
@@ -0,0 +1,8 @@
+// lib/config/api_config.dart
+// ⚠️ ในโปรเจกต์จริงให้ใช้ Environment Variables
+// ไฟล์นี้เพิ่มใน .gitignore
+
+class ApiConfig {
+  // TODO: แทนที่ด้วย API Key ของคุณ
+  static const String geminiApiKey = '                                                       ';
+}
